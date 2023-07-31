b2c5d7ce16a8d66c183a7e32afa7fbdd69ba0cf1
yanghao05 <yanghao05@qianxin.com>
2023-07-31 17:17:26 +0800

    first commit

---
+++ engine.go
@@ -0,0 +1,73 @@
+package httpGin
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/rogeecn/atom-addons/providers/log"
+	"github.com/rogeecn/atom/container"
+	"github.com/rogeecn/atom/contracts"
+	"github.com/rogeecn/atom/utils/opt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func DefaultProvider() container.ProviderContainer {
+	return container.ProviderContainer{
+		Provider: Provide,
+		Options: []opt.Option{
+			opt.Prefix(DefaultPrefix),
+		},
+	}
+}
+
+type Service struct {
+	conf   *Config
+	Engine *gin.Engine
+}
+
+func (e *Service) Use(middleware ...gin.HandlerFunc) gin.IRoutes {
+	return e.Engine.Use(middleware...)
+}
+
+func (e *Service) GetEngine() interface{} {
+	return e.Engine
+}
+
+func (e *Service) Serve() error {
+	if e.conf.Tls != nil {
+		return e.Engine.RunTLS(e.conf.PortString(), e.conf.Tls.Cert, e.conf.Tls.Key)
+	}
+	return e.Engine.Run(e.conf.PortString())
+}
+
+func Provide(opts ...opt.Option) error {
+	o := opt.New(opts...)
+	var config Config
+	if err := o.UnmarshalConfig(&config); err != nil {
+		return err
+	}
+
+	return container.Container.Provide(func() (contracts.HttpService, error) {
+		gin.DefaultWriter = log.LevelWriter{Level: log.InfoLevel}
+		gin.DefaultErrorWriter = log.LevelWriter{Level: log.ErrorLevel}
+
+		engine := gin.New()
+		engine.Use(gin.Recovery())
+		engine.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+			return fmt.Sprintf(`%s - [%s] "%s %s %s %d %s '%q' %s"\n`,
+				param.ClientIP,
+				param.TimeStamp.Format(time.RFC1123),
+				param.Method,
+				param.Path,
+				param.Request.Proto,
+				param.StatusCode,
+				param.Latency,
+				param.Request.UserAgent(),
+				param.ErrorMessage,
+			)
+		}))
+
+		return &Service{Engine: engine, conf: &config}, nil
+	}, o.DiOptions()...)
+}
